f6f27f0077161240986197223bf1c06948283f4e
anashamimi <anashamimi2@gmail.com>
2023-05-16 19:43:01 +0200

    add class movie

---
+++ Models/Movie.php
@@ -0,0 +1,26 @@
+<?php
+
+class Movie
+{
+    public $id;
+    public $title;
+    public $original_title;
+    public $nationality;
+    public $date;
+    public $vote;
+    public $image;
+    public $category;
+
+    public function __construct($id, $title, $original_title, 
+    $nationality, $date, $vote, $image, $category)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->original_title = $original_title;
+        $this->nationality = $nationality;
+        $this->date = $date;
+        $this->vote = $vote;
+        $this->image = $image;
+        $this->category = $category;        
+    }
+}
